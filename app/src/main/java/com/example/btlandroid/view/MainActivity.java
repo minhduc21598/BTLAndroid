c47def888f48986e53e2f28bba9f5a71821f84e2
= <=>
2020-06-25 22:49:50 +0700

    sign up

--- app/src/main/java/com/example/btlandroid/view/MainActivity.java
+++ app/src/main/java/com/example/btlandroid/view/MainActivity.java
@@ -1,16 +1,31 @@
 package com.example.btlandroid.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.btlandroid.R;
 
 public class MainActivity extends AppCompatActivity {
+    Button btSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btSignUp = (Button) findViewById(R.id.btSignUp);
+        btSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivitySignUp();
+            }
+        });
+    }
+    public void openActivitySignUp() {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
